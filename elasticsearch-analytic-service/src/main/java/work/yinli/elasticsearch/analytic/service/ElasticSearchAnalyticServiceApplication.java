03e1b2d941899e68015f49ce163299ff8a6f8acb
ahian <ahianzhang@gmail.com>
2024-04-07 16:10:15 +0800

    feat: :tada:

---
+++ elasticsearch-analytic-service/src/main/java/work/yinli/elasticsearch/analytic/service/ElasticSearchAnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package work.yinli.elasticsearch.analytic.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchAnalyticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchAnalyticServiceApplication.class, args);
+	}
+
+}
---
+++ elasticsearch-analytic-service/src/test/java/work/yinli/elasticsearch/analytic/service/ElasticsearchAnalysicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package work.yinli.elasticsearch.analytic.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchAnalyticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
