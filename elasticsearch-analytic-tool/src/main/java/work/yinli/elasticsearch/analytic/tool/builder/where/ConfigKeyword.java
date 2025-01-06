91d87591785e96e99093559836464bcf605c5600
AhianZhang <augerta1995@gmail.com>
2025-01-06 14:38:08 +0800

    fix: add missed data

---
+++ elasticsearch-analytic-tool/src/main/java/work/yinli/elasticsearch/analytic/tool/builder/where/ConfigKeyword.java
@@ -0,0 +1,11 @@
+package work.yinli.elasticsearch.analytic.tool.builder.where;
+
+/**
+ * 配置类关键字、保留字
+ */
+public class ConfigKeyword {
+    public static final boolean NEST_SCORE_MODE = false;
+    public static final int MIN_MATCH = 0;
+    public static final String MAX_GAPS = "maxGaps";
+    public static final String ORDERED = "ordered";
+}
